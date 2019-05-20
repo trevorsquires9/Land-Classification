2900b6b7b1f8f2e5de69444b8e2d82443d744af4
Trevor Squires <tsquire@clemson.edu>
2019-05-20 10:55:54 -0400

    Added ensemble functionality

--- simpleClassifier.py
+++ simpleClassifier.py
@@ -15,9 +15,14 @@
 #       - Need to figure out how to read in .tiff files and preprocess
 #       - Make sure to read data in small chunks
 #       - Would like confusion matrix to be on testing set instead of training
-#        - 
+#       - If there is a tie for best classification, ensemble currently chooses
+#         the lowest classification (definitely not optimal)
 #
 # =============================================================================
+# Necessary Packages
+# =============================================================================
+from __future__ import division
+import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
@@ -26,28 +31,33 @@ from sklearn import linear_model
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from Tkinter import *
+
 # =============================================================================
-## Define functions for GUI
+# Define functions for GUI
+# =============================================================================
 def getMainData():
     master.destroy()
-    if svmRun.get():
+    global svmDo,sgdDo,rfDo,mlpDo
+    svmDo = svmRun.get()
+    sgdDo = sgdRun.get()
+    rfDo = rfRun.get()
+    mlpDo = mlpRun.get()
+    if svmDo:
         svmMaster = Tk()
         Label(svmMaster, text="Select your Kernel Function:").grid(row=0,column=0)
         kernelLabel = StringVar(svmMaster)
         kernelLabel.set('rbf')
         selectKernel = OptionMenu(svmMaster, kernelLabel,'rbf','poly','linear')
         selectKernel.grid(row=0,column=1)
-        svmVerbose = IntVar() 
-        Checkbutton(svmMaster, text='Verbose', variable=svmVerbose).grid(row=1,column=0, sticky=W) 
-        svmCommand = lambda : runSVM(svmMaster,kernelLabel,svmVerbose)
+        svmCommand = lambda : runSVM(svmMaster,kernelLabel)
         Button(svmMaster,text = "Continue",command = svmCommand).grid(row=5)
         svmMaster.mainloop()
         
-    if sgdRun.get():
+    if sgdDo:
         sgdMaster = Tk()
         sgdAlert = Toplevel(sgdMaster)
         sgdAlert.title = 'Warning'
-        sgdmsg = Message(sgdAlert,text='Warning: SGD is a linear classifier and should not be relied upon for nonlinear classification')
+        sgdmsg = Message(sgdAlert,text='Warning: SGD is only a linear classifier and should not be relied upon for complicated classification')
         sgdmsg.pack()
         
         Label(sgdMaster, text="Select your Loss Function:").grid(row=0,column=0)
@@ -55,26 +65,22 @@ def getMainData():
         lossLabel.set('l2')
         selectLoss = OptionMenu(sgdMaster, lossLabel,'l2','l1','elasticnet')
         selectLoss .grid(row=0,column=1)
-        sgdVerbose = IntVar() 
-        Checkbutton(sgdMaster, text='Verbose', variable=sgdVerbose).grid(row=1,column=0, sticky=W) 
-        sgdCommand = lambda : runSGD(sgdMaster,lossLabel,sgdVerbose)
+        sgdCommand = lambda : runSGD(sgdMaster,lossLabel)
         Button(sgdMaster,text = "Continue",command = sgdCommand).grid(row=5)
         sgdMaster.mainloop()
         
-    if rfRun.get():
+    if rfDo:
         rfMaster = Tk()
         Label(rfMaster, text="Indicate the number of trees to use:").grid(row=0,column=0)
         treeNum = IntVar(rfMaster)
         treeNum.set(256)
         selectTree = OptionMenu(rfMaster, treeNum,32,64,128,256,512,1024,2048)
         selectTree.grid(row=0,column=1)
-        rfVerbose = IntVar() 
-        Checkbutton(rfMaster, text='Verbose', variable=rfVerbose).grid(row=1,column=0, sticky=W) 
-        rfCommand = lambda : runRF(rfMaster,treeNum,rfVerbose)
+        rfCommand = lambda : runRF(rfMaster,treeNum)
         Button(rfMaster,text = "Continue",command = rfCommand).grid(row=5)
         rfMaster.mainloop()
     
-    if mlpRun.get():
+    if mlpDo:
         mlpMaster = Tk()
         Label(mlpMaster, text="Indicate the number of hidden layers to use:").grid(row=0,column=0)
         layerNum = IntVar(mlpMaster)
@@ -86,72 +92,58 @@ def getMainData():
         neuronNum.set(25)
         selectLayer = OptionMenu(mlpMaster, neuronNum,10,25,50,100,250,500)
         selectLayer.grid(row=1,column=1)
-        mlpVerbose = IntVar() 
-        Checkbutton(mlpMaster, text='Verbose', variable=mlpVerbose).grid(row=2,column=0, sticky=W) 
-        mlpCommand = lambda : runMLP(mlpMaster,layerNum,neuronNum,mlpVerbose)
+        mlpCommand = lambda : runMLP(mlpMaster,layerNum,neuronNum)
         Button(mlpMaster,text = "Continue",command = mlpCommand).grid(row=5)
         mlpMaster.mainloop()
-        
-def runSVM(svmMaster,kernelLabel,svmVerbose,):
+    
+    print 'Running the following classifiers:'
+    if svmDo:
+        print '\t- Support Vector Machine with an ' + kernelLabel.get() + ' kernel'
+    if sgdDo:
+        print '\t- Stochastic Gradient Descent with ' + lossLabel.get() + ' loss function'
+    if rfDo:
+        print '\t- Random Forest with ' + str(treeNum.get()) + ' trees'
+    if mlpDo:
+        print '\t- Multi-Layered Perception with ' + str(layerNum.get()) + ' layers and ' + str(neuronNum.get()) + ' neurons in each layer\n'
+    if svmDo:
+        svmclf.fit(Xtrain,yTrain)
+    if sgdDo:
+        sgdclf.fit(Xtrain,yTrain)
+    if rfDo:
+        rfclf.fit(Xtrain,yTrain)
+    if mlpDo:
+        mlpclf.fit(Xtrain,yTrain)
+                
+def runSVM(svmMaster,kernelLabel,):
     svmKernel = kernelLabel.get()
     svmMaster.destroy()
     global svmclf
     svmclf = svm.SVC(gamma='scale',kernel=svmKernel)
-    svmclf.fit(X,y)
-    if svmVerbose.get():
-        df = pd.DataFrame()
-        df['truth'] = y
-        df['predict'] = svmclf.predict(X)
-        print "\nClassifier: Support Vector Machine\n \tKernel: " + svmKernel + "\n\tConfusion Matrix:\n"
-        print(pd.crosstab(df['truth'], df['predict'], margins=True))
 
-def runSGD(sgdMaster,lossLabel,sgdVerbose,):
+def runSGD(sgdMaster,lossLabel,):
     sgdLoss = lossLabel.get()
     sgdMaster.destroy()
     global sgdclf
-    sgdclf = linear_model.SGDClassifier(max_iter=5000,penalty=sgdLoss,n_iter_no_change=250,tol=1e-7)
-    sgdclf.fit(X,y)
-    if sgdVerbose.get():
-        df = pd.DataFrame()
-        df['truth'] = y
-        df['predict'] = sgdclf.predict(X)
-        print "\nClassifier: Stochastic Gradient Descent\n \tLoss function: " + sgdLoss + "\n\tConfusion Matrix:\n"
-        print(pd.crosstab(df['truth'], df['predict'], margins=True))
+    sgdclf = linear_model.SGDClassifier(max_iter=5000,penalty=sgdLoss,n_iter_no_change=50,tol=1e-7)
 
-def runRF(rfMaster,treeNum,rfVerbose,):
+def runRF(rfMaster,treeNum,):
     rfTree = treeNum.get()
     rfMaster.destroy()
     global rfclf
-    rfclf = RandomForestClassifier(n_estimators = rfTree, oob_score=rfVerbose)
-    rfclf.fit(X,y)
-    if rfVerbose.get():
-        df = pd.DataFrame()
-        df['truth'] = y
-        df['predict'] = rfclf.predict(X)
-        print "\nClassifier: Random Forest\n \tNumber of Trees: " + str(rfTree) + "\n\tOut of Bag Accuracy: " + str(rfclf.oob_score_)
-        print "\n\tFeature importances:"
-        for i in range(17):
-            print '\t\tFeature ' + str(i+1) + ' has importance ' + str(rfclf.feature_importances_[i+1])
-        print('\n\tConfusion Matrix')
-        print(pd.crosstab(df['truth'], df['predict'], margins=True))
-        
-def runMLP(mlpMaster,layerNum,neuronNum,mlpVerbose,):
+    rfclf = RandomForestClassifier(n_estimators = rfTree, oob_score=True)
+
+def runMLP(mlpMaster,layerNum,neuronNum,):
     mlpLayer = layerNum.get()
     mlpNeuron = neuronNum.get()
     mlpMaster.destroy()
     global mlpclf
     layers = np.ones(mlpLayer,dtype = np.int)*mlpNeuron
     mlpclf = MLPClassifier(solver='adam',hidden_layer_sizes = layers.tolist(),max_iter=4000,tol=1e-8)
-    mlpclf.fit(X,y)
-    if mlpVerbose.get():
-        df = pd.DataFrame()
-        df['truth'] = y
-        df['predict'] = mlpclf.predict(X)
-        print "\nClassifier: Multi-Layered Perceptron\n\tNumber of Hidden Layers: " + str(mlpLayer) + "\n\tNumber of Neurons: " + str(mlpNeuron)+"\n\tConfusion Matrix:\n"
-        print(pd.crosstab(df['truth'], df['predict'], margins=True))
+
 
 # =============================================================================
-## Initialization of data - to be changed later
+# Initialization of data - to be changed later
+# =============================================================================
 m = 80
 n = 200
 image = np.random.rand(m,n,18)
@@ -160,16 +152,23 @@ train = np.random.randint(20,size=(m,n))
 sampleNum = (train>10).sum()
 labels = np.unique(train[train>10])
 
-X = image[train>10,:]
-y = train[train>10]
+
+Xall = image[train>10,:]
+testSize = np.floor((Xall.shape[0]*3)/4)
+
+Xtrain = Xall[0:int(testSize),:]
+Xtest = Xall[Xtrain.shape[0]:Xall.shape[0],:]
+
+yAll = train[train>10]
+yTrain = yAll[0:int(testSize)]
+yTest = yAll[Xtrain.shape[0]:Xall.shape[0]]
 
 newShape = (image.shape[0]*image.shape[1], image.shape[2])
 imageVectorized = image.reshape(newShape)
-# =============================================================================
-
 
 # =============================================================================
-## Define the GUI used for data
+# Define the GUI used for obtaining
+# =============================================================================
 master = Tk()
 Label(master, text="Image to classify (should be .tif file)").grid(row=0)
 Label(master, text="Labeled data ").grid(row=1)
@@ -192,11 +191,99 @@ Checkbutton(master, text='MLP', variable=mlpRun).grid(row=4,column=1, sticky=W)
 Button(master, text='Quit', command=master.quit).grid(row=5, column=0, sticky=W, pady=4)
 Button(master, text='Continue', command=getMainData).grid(row=5, column=1, sticky=W, pady=4)
 
-mainloop( )
+mainloop()
 
     
+# =============================================================================
+# Compute summary statistics and other analysis
+# =============================================================================    
+print '\nSummary Statistics'
 
+if svmDo:
+    svmTestPred = svmclf.predict(Xtest)
+    svmTestAcc = (svmTestPred==yTest).sum() / yTest.size
+    df = pd.DataFrame()
+    df['truth'] = yTest
+    df['predict'] = svmTestPred
+    print '\nSupport Vector Machine'
+    print 'Out of bag accuracy: ' + str(svmTestAcc*100)
+    print 'Confusion Matrix'
+    print(pd.crosstab(df['truth'], df['predict'], margins=True)) 
+if sgdDo:
+    sgdTestPred = sgdclf.predict(Xtest)
+    sgdTestAcc = (sgdTestPred==yTest).sum() / yTest.size
+    df = pd.DataFrame()
+    df['truth'] = yTest
+    df['predict'] = sgdTestPred
+    print '\nStochastic Gradient Descent'
+    print 'Out of bag accuracy: ' + str(sgdTestAcc*100)
+    print 'Confusion Matrix'
+    print(pd.crosstab(df['truth'], df['predict'], margins=True)) 
+if rfDo:
+    rfTestPred = rfclf.predict(Xtest)
+    rfTestAcc = (rfTestPred==yTest).sum() / yTest.size
+    df = pd.DataFrame()
+    df['truth'] = yTest
+    df['predict'] = rfTestPred
+    print '\nRandom Forest'
+    print 'Out of bag accuracy: ' + str(rfTestAcc*100)
+    print 'Feature importances:'
+    for i in range(17):
+        print '\tFeature ' + str(i+1) + ' has importance ' + str(rfclf.feature_importances_[i+1])
+    print 'Confusion Matrix'
+    print(pd.crosstab(df['truth'], df['predict'], margins=True)) 
+if mlpDo:
+    mlpTestPred = mlpclf.predict(Xtest)
+    mlpTestAcc = (mlpTestPred==yTest).sum() / yTest.size
+    df = pd.DataFrame()
+    df['truth'] = yTest
+    df['predict'] = mlpTestPred
+    print '\nMulti-Layered Perceptron'
+    print 'Out of bag accuracy: ' + str(mlpTestAcc*100)
+    print 'Confusion Matrix'
+    print(pd.crosstab(df['truth'], df['predict'], margins=True))     
 
+# =============================================================================
+# Ensemble Techniques
+# =============================================================================    
+ensembleDialogue = Tk()
+Label(ensembleDialogue, text='Based on summary statistics, select which of the following classifiers you would like to use.  (Note: selecting more than one will result in an ensemble classification)').grid(row=0)
 
+svmUse = IntVar()
+sgdUse = IntVar()
+rfUse = IntVar()
+mlpUse = IntVar() 
+Checkbutton(ensembleDialogue, text='SVM', variable=svmUse).grid(row=1, sticky=W) 
+Checkbutton(ensembleDialogue, text='SGD', variable=sgdUse).grid(row=2, sticky=W) 
+Checkbutton(ensembleDialogue, text='Random Forest', variable=rfUse).grid(row=3, sticky=W) 
+Checkbutton(ensembleDialogue, text='MLP', variable=mlpUse).grid(row=4, sticky=W) 
+Button(ensembleDialogue,text = "Continue",command = ensembleDialogue.destroy).grid(row=5)
+ensembleDialogue.mainloop()
 
+ensemble = [svmUse.get(), sgdUse.get(), rfUse.get(), mlpUse.get()]
+pred = np.zeros((imageVectorized.shape[0],4))
 
+if svmUse.get():
+    svmPred = svmclf.predict(imageVectorized)
+    pred[:,0] = svmPred
+if sgdUse.get():
+    sgdPred = sgdclf.predict(imageVectorized)
+    pred[:,1] = sgdPred
+if rfUse.get():
+    rfPred = rfclf.predict(imageVectorized)
+    pred[:,2] = rfPred
+if mlpUse.get():
+    mlpPred = mlpclf.predict(imageVectorized)
+    pred[:,3] = mlpPred
+    
+pred = pred[:,np.nonzero(ensemble)]    
+
+ensemblePred = np.zeros(imageVectorized.shape[0]) 
+count = 0
+
+for i in pred:
+    ensemblePred[count] = scipy.stats.mode(i,axis=None)[0]
+    count = count + 1
+    
+    
+    
